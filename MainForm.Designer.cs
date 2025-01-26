a68aa00dc02e1e530d54473a5d3c8b22e71ca4a4
Syobosyobonn <154320513+syobosyobonn@users.noreply.github.com>
2025-01-26 23:29:47 +0900

    (`・ω・´)<1.0.1

---
+++ MainForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace DTMFEmulator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button1 = new Button();
+            Button2 = new Button();
+            Button3 = new Button();
+            Button4 = new Button();
+            Button5 = new Button();
+            Button6 = new Button();
+            Button7 = new Button();
+            Button8 = new Button();
+            Button9 = new Button();
+            Button0 = new Button();
+            ButtonStar = new Button();
+            ButtonHash = new Button();
+            ToneNumericUpDown1 = new NumericUpDown();
+            ToneNumericUpDown2 = new NumericUpDown();
+            CustomToneButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)ToneNumericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)ToneNumericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // Button1
+            // 
+            Button1.ForeColor = SystemColors.WindowText;
+            Button1.Location = new Point(0, 0);
+            Button1.Name = "Button1";
+            Button1.Size = new Size(75, 75);
+            Button1.TabIndex = 0;
+            Button1.Text = "1";
+            Button1.UseVisualStyleBackColor = true;
+            Button1.MouseDown += Button_MouseDown;
+            Button1.MouseMove += Button_MouseMove;
+            Button1.MouseUp += Button_MouseUp;
+            // 
+            // Button2
+            // 
+            Button2.ForeColor = SystemColors.WindowText;
+            Button2.Location = new Point(75, 0);
+            Button2.Name = "Button2";
+            Button2.Size = new Size(75, 75);
+            Button2.TabIndex = 1;
+            Button2.Text = "2";
+            Button2.UseVisualStyleBackColor = true;
+            Button2.MouseDown += Button_MouseDown;
+            Button2.MouseMove += Button_MouseMove;
+            Button2.MouseUp += Button_MouseUp;
+            // 
+            // Button3
+            // 
+            Button3.ForeColor = SystemColors.WindowText;
+            Button3.Location = new Point(150, 0);
+            Button3.Name = "Button3";
+            Button3.Size = new Size(75, 75);
+            Button3.TabIndex = 2;
+            Button3.Text = "3";
+            Button3.UseVisualStyleBackColor = true;
+            Button3.MouseDown += Button_MouseDown;
+            Button3.MouseMove += Button_MouseMove;
+            Button3.MouseUp += Button_MouseUp;
+            // 
+            // Button4
+            // 
+            Button4.ForeColor = SystemColors.WindowText;
+            Button4.Location = new Point(0, 75);
+            Button4.Name = "Button4";
+            Button4.Size = new Size(75, 75);
+            Button4.TabIndex = 3;
+            Button4.Text = "4";
+            Button4.UseVisualStyleBackColor = true;
+            Button4.MouseDown += Button_MouseDown;
+            Button4.MouseMove += Button_MouseMove;
+            Button4.MouseUp += Button_MouseUp;
+            // 
+            // Button5
+            // 
+            Button5.ForeColor = SystemColors.WindowText;
+            Button5.Location = new Point(75, 75);
+            Button5.Name = "Button5";
+            Button5.Size = new Size(75, 75);
+            Button5.TabIndex = 4;
+            Button5.Text = "5";
+            Button5.UseVisualStyleBackColor = true;
+            Button5.MouseDown += Button_MouseDown;
+            Button5.MouseMove += Button_MouseMove;
+            Button5.MouseUp += Button_MouseUp;
+            // 
+            // Button6
+            // 
+            Button6.ForeColor = SystemColors.WindowText;
+            Button6.Location = new Point(150, 75);
+            Button6.Name = "Button6";
+            Button6.Size = new Size(75, 75);
+            Button6.TabIndex = 5;
+            Button6.Text = "6";
+            Button6.UseVisualStyleBackColor = true;
+            Button6.MouseDown += Button_MouseDown;
+            Button6.MouseMove += Button_MouseMove;
+            Button6.MouseUp += Button_MouseUp;
+            // 
+            // Button7
+            // 
+            Button7.ForeColor = SystemColors.WindowText;
+            Button7.Location = new Point(0, 150);
+            Button7.Name = "Button7";
+            Button7.Size = new Size(75, 75);
+            Button7.TabIndex = 6;
+            Button7.Text = "7";
+            Button7.UseVisualStyleBackColor = true;
+            Button7.MouseDown += Button_MouseDown;
+            Button7.MouseMove += Button_MouseMove;
+            Button7.MouseUp += Button_MouseUp;
+            // 
+            // Button8
+            // 
+            Button8.ForeColor = SystemColors.WindowText;
+            Button8.Location = new Point(75, 150);
+            Button8.Name = "Button8";
+            Button8.Size = new Size(75, 75);
+            Button8.TabIndex = 7;
+            Button8.Text = "8";
+            Button8.UseVisualStyleBackColor = true;
+            Button8.MouseDown += Button_MouseDown;
+            Button8.MouseMove += Button_MouseMove;
+            Button8.MouseUp += Button_MouseUp;
+            // 
+            // Button9
+            // 
+            Button9.ForeColor = SystemColors.WindowText;
+            Button9.Location = new Point(150, 150);
+            Button9.Name = "Button9";
+            Button9.Size = new Size(75, 75);
+            Button9.TabIndex = 8;
+            Button9.Text = "9";
+            Button9.UseVisualStyleBackColor = true;
+            Button9.MouseDown += Button_MouseDown;
+            Button9.MouseMove += Button_MouseMove;
+            Button9.MouseUp += Button_MouseUp;
+            // 
+            // Button0
+            // 
+            Button0.ForeColor = SystemColors.WindowText;
+            Button0.Location = new Point(75, 225);
+            Button0.Name = "Button0";
+            Button0.Size = new Size(75, 75);
+            Button0.TabIndex = 9;
+            Button0.Text = "0";
+            Button0.UseVisualStyleBackColor = true;
+            Button0.MouseDown += Button_MouseDown;
+            Button0.MouseMove += Button_MouseMove;
+            Button0.MouseUp += Button_MouseUp;
+            // 
+            // ButtonStar
+            // 
+            ButtonStar.ForeColor = SystemColors.WindowText;
+            ButtonStar.Location = new Point(0, 225);
+            ButtonStar.Name = "ButtonStar";
+            ButtonStar.Size = new Size(75, 75);
+            ButtonStar.TabIndex = 10;
+            ButtonStar.Text = "*";
+            ButtonStar.UseVisualStyleBackColor = true;
+            ButtonStar.MouseDown += Button_MouseDown;
+            ButtonStar.MouseMove += Button_MouseMove;
+            ButtonStar.MouseUp += Button_MouseUp;
+            // 
+            // ButtonHash
+            // 
+            ButtonHash.ForeColor = SystemColors.WindowText;
+            ButtonHash.Location = new Point(150, 225);
+            ButtonHash.Name = "ButtonHash";
+            ButtonHash.Size = new Size(75, 75);
+            ButtonHash.TabIndex = 11;
+            ButtonHash.Text = "#";
+            ButtonHash.UseVisualStyleBackColor = true;
+            ButtonHash.MouseDown += Button_MouseDown;
+            ButtonHash.MouseMove += Button_MouseMove;
+            ButtonHash.MouseUp += Button_MouseUp;
+            // 
+            // ToneNumericUpDown1
+            // 
+            ToneNumericUpDown1.ForeColor = SystemColors.WindowText;
+            ToneNumericUpDown1.Location = new Point(0, 301);
+            ToneNumericUpDown1.Maximum = new decimal(new int[] { 5000, 0, 0, 0 });
+            ToneNumericUpDown1.Minimum = new decimal(new int[] { 350, 0, 0, 0 });
+            ToneNumericUpDown1.Name = "ToneNumericUpDown1";
+            ToneNumericUpDown1.Size = new Size(75, 23);
+            ToneNumericUpDown1.TabIndex = 12;
+            ToneNumericUpDown1.Value = new decimal(new int[] { 400, 0, 0, 0 });
+            // 
+            // ToneNumericUpDown2
+            // 
+            ToneNumericUpDown2.ForeColor = SystemColors.WindowText;
+            ToneNumericUpDown2.Location = new Point(150, 301);
+            ToneNumericUpDown2.Maximum = new decimal(new int[] { 5000, 0, 0, 0 });
+            ToneNumericUpDown2.Minimum = new decimal(new int[] { 16, 0, 0, 0 });
+            ToneNumericUpDown2.Name = "ToneNumericUpDown2";
+            ToneNumericUpDown2.Size = new Size(75, 23);
+            ToneNumericUpDown2.TabIndex = 13;
+            ToneNumericUpDown2.Value = new decimal(new int[] { 20, 0, 0, 0 });
+            // 
+            // CustomToneButton
+            // 
+            CustomToneButton.ForeColor = SystemColors.WindowText;
+            CustomToneButton.Location = new Point(75, 301);
+            CustomToneButton.Name = "CustomToneButton";
+            CustomToneButton.Size = new Size(75, 23);
+            CustomToneButton.TabIndex = 14;
+            CustomToneButton.Text = "Custom";
+            CustomToneButton.UseVisualStyleBackColor = true;
+            CustomToneButton.MouseDown += Button_MouseDown;
+            CustomToneButton.MouseMove += Button_MouseMove;
+            CustomToneButton.MouseUp += Button_MouseUp;
+            // 
+            // MainForm
+            // 
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(225, 324);
+            Controls.Add(CustomToneButton);
+            Controls.Add(ToneNumericUpDown2);
+            Controls.Add(ToneNumericUpDown1);
+            Controls.Add(ButtonHash);
+            Controls.Add(ButtonStar);
+            Controls.Add(Button0);
+            Controls.Add(Button9);
+            Controls.Add(Button8);
+            Controls.Add(Button7);
+            Controls.Add(Button6);
+            Controls.Add(Button5);
+            Controls.Add(Button4);
+            Controls.Add(Button3);
+            Controls.Add(Button2);
+            Controls.Add(Button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MaximumSize = new Size(241, 363);
+            MdiChildrenMinimizedAnchorBottom = false;
+            MinimizeBox = false;
+            MinimumSize = new Size(241, 363);
+            Name = "MainForm";
+            ShowIcon = false;
+            Text = "DTMFEmulator";
+            ((System.ComponentModel.ISupportInitialize)ToneNumericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)ToneNumericUpDown2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Button Button7;
+        private System.Windows.Forms.Button Button8;
+        private System.Windows.Forms.Button Button9;
+        private System.Windows.Forms.Button Button0;
+        private System.Windows.Forms.Button ButtonStar;
+        private System.Windows.Forms.Button ButtonHash;
+        private System.Windows.Forms.NumericUpDown ToneNumericUpDown1;
+        private System.Windows.Forms.NumericUpDown ToneNumericUpDown2;
+        private System.Windows.Forms.Button CustomToneButton;
+    }
+}
